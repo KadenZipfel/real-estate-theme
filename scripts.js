45c3347d96bc520f53bb75001850ec05acc8c2ab
unknown <kaden.zipfel@hotmail.com>
2018-12-02 10:51:48 -0800

    Setup featured card info on hover

--- scripts.js
+++ scripts.js
@@ -2,17 +2,17 @@ var cardRow1 = document.querySelector('.card-row-1');
 var cardRow2 = document.querySelector('.card-row-2');
 
 function leftScrollOne() {
-  cardRow1.scrollLeft += -400;
+  cardRow1.scrollLeft += -450;
 }
 
 function rightScrollOne() {
-  cardRow1.scrollLeft += 400;
+  cardRow1.scrollLeft += 450;
 }
 
 function leftScroll2() {
-  cardRow2.scrollLeft += -400;
+  cardRow2.scrollLeft += -450;
 }
 
 function rightScroll2() {
-  cardRow2.scrollLeft += 400;
+  cardRow2.scrollLeft += 450;
 }
